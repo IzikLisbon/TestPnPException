add695959bd4b1415199fc17492fcd5560d547f7
Izik Lisbon <izikl@microsoft.com>
2015-12-21 17:27:09 -0800

    Initial Commit

---
+++ App31/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App31
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            string ver = await PlatformHacks.GetWindowsVersion();
+
+            Debug.WriteLine(ver);
+            // Create the message dialog and set its content
+            var messageDialog = new MessageDialog(ver);
+
+            //// Add commands and set their callbacks; both buttons use the same callback function instead of inline event handlers
+
+            messageDialog.Commands.Add(new UICommand(
+                "Close"));
+
+            // Set the command to be invoked when escape is pressed
+            messageDialog.CancelCommandIndex = 0;
+
+            // Show the message dialog
+            await messageDialog.ShowAsync();
+
+        }
+
+    }
+}
